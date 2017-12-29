dc43c32527453ce42035c2b41e06f7efcdb6a76d
crism <crismarfr@free.fr>
2017-12-29 17:15:35 +0100

    test

--- annuaire/src/main/java/com/example/demo/annuaire/AnnuaireApplication.java
+++ annuaire/src/main/java/com/example/demo/annuaire/AnnuaireApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo.annuaire;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -9,7 +11,6 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 @EnableHystrix
 @EnableEurekaServer
 public class AnnuaireApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AnnuaireApplication.class, args);
 	}
