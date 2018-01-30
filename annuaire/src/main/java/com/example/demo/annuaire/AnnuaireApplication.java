7c37e8d0f7c4f73b40ed69925017f75dfd0fde73
crism <crismarfr@free.fr>
2018-01-30 11:52:39 +0100

    commit

--- annuaire/src/main/java/com/example/demo/annuaire/AnnuaireApplication.java
+++ annuaire/src/main/java/com/example/demo/annuaire/AnnuaireApplication.java
@@ -1,7 +1,5 @@
 package com.example.demo.annuaire;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
