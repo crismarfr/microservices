361e1b363186394e7b43ee58dcf8cac3eb49bca1
crism <crismarfr@free.fr>
2018-09-18 16:19:13 +0200

    Init

---
+++ microservice5/src/main/java/com/example/demo/DemoApplication5.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.bean.Message;
+
+
+@SpringBootApplication
+@RestController
+public class DemoApplication5{
+
+	@RequestMapping(value="/callMicroservice", method=RequestMethod.GET)
+	public Message callMicroservice(){
+		Message mess=new Message();
+		mess.setMess("Status OK microservice5 !");
+		return mess;
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication5.class, args);
+	}
+
+}
