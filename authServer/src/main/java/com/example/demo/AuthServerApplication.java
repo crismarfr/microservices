9ed7c0bbe42c8939e1b4575d2384802f1a31f300
crism <crismarfr@free.fr>
2018-12-08 15:04:58 +0100

    test

--- authServer/src/main/java/com/example/demo/AuthServerApplication.java
+++ authServer/src/main/java/com/example/demo/AuthServerApplication.java
@@ -4,6 +4,7 @@ import java.security.Principal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
