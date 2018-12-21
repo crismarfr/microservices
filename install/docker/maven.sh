f6f3fbdb01a3d107657a497c9750d593fb503d96
arom <arom@localhost.localdomain>
2018-12-21 17:07:28 +0100

    test

--- install/docker/maven.sh
+++ install/docker/maven.sh
@@ -1,26 +1,26 @@
 cd ../../annuaire
-mvn clean package
+mvn clean package -DskipTests
 cd ../../hystrix
-mvn clean package
+mvn clean package -DskipTests
 cd ../../microservice1
-mvn clean package
+mvn clean package -DskipTests
 cd ../../microservice3  
-mvn clean package
+mvn clean package -DskipTests
 cd ../../microservice5 
-mvn clean package
+mvn clean package -DskipTests
 cd ../../authServer  
-mvn clean package
+mvn clean package -DskipTests
 cd ../../configServer
-mvn clean package
+mvn clean package -DskipTests
 cd ../../gateway 
-mvn clean package
+mvn clean package -DskipTests
 cd ../../microservice2 
-mvn clean package
+mvn clean package -DskipTests
 cd ../../microservice4
-mvn clean package
+mvn clean package -DskipTests
 cd ../../microservice6
-mvn clean package
+mvn clean package -DskipTests
 cd ../../reactMicroservice
-mvn clean package
+mvn clean package -DskipTests
 cd ../../zipkin
-mvn clean package
+mvn clean package -DskipTests
